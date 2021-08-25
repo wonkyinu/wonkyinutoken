2012a146cd66dc613c5fbc8ed7eadf5ceaf2c759
wonkyinu <wlx.eugene.spotify@gmail.com>
2021-08-26 00:36:40 +0800

    test

--- migrations/2_deploy_token.js
+++ migrations/2_deploy_token.js
@@ -1,5 +1,10 @@
 const WonkyInuToken = artifacts.require("WonkyInuToken");
+var WonkyInuTokenSale = artifacts.require("./WonkyInuTokenSale.sol");
 
-module.exports = function (deployer) {
-  deployer.deploy(WonkyInuToken, 1000000);
+module.exports = function(deployer) {
+  deployer.deploy(WonkyInuToken, 1000000).then(function() {
+    // Token price is 0.001 Ether
+    var tokenPrice = 1000000000000000;
+    return deployer.deploy(WonkyInuTokenSale, WonkyInuToken.address, tokenPrice);
+  });
 };
